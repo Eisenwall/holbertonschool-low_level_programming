631ff0075b6189f9bdeba0bc77b2ae64be6d5318
Eisenwall <Eisenwall@proton.me>
2025-06-13 11:58:08 +0000

    HTS

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,11 @@
  */
 int main(void)
 {
-    char c;
+	char c;
 
-    for (c = '0'; c <= '9'; c++)
-        putchar(c);
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	putchar('\n');
 
-    putchar('\n');
-
-    return (0);
+	return (0);
 }
