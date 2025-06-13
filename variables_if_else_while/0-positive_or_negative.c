bae5bb44ffc494c8858a1e150eea1a71627dda20
Eisenwall <Eisenwall@proton.me>
2025-06-13 11:34:21 +0000

    HTS

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -8,12 +8,13 @@ int main(void){
     n = rand() - RAND_MAX / 2;
     
     if (n > 0){
-        printf('is positive');
+        printf("is positive");
     }
-    else if (n = 0){
+    else if (n == 0){
         printf("is zero");
     }
     else{
-        printf('is negative');
+        printf("is negative");
     }
+    return 0;
 }
