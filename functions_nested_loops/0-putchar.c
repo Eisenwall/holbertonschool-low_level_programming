0add27414c6962f28f31a97c11543c41e59321f9
Eisenwall <Eisenwall@proton.me>
2025-06-13 13:07:55 +0000

    HTS

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,18 +3,17 @@
 /**
  * main - prints _putchar followed by a new line
  *
- * Return: 0
+ * Return: 0 on success
  */
 int main(void)
 {
     char str[] = "_putchar\n";
     int i = 0;
 
-    while (str[i] != '\0')
+    while (str[i])
     {
-        putchar(str[i]);
+        write(1, &str[i], 1);
         i++;
     }
-
     return (0);
 }
