a6484bff54d6bb71ad0a2363b5b453c40c29e61e
Eisenwall <Eisenwall@proton.me>
2025-06-13 12:22:27 +0000

    HTS

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 
 /**
- * main - prints the lowercase alphabet in reverse
- *
+ * Hi
+ *But
  * Return: 0
  */
 int main(void)
 {
-    char c;
-    for (c = '0'; c <= '9'; c++)
-        putchar(c);
-    for (c = 'a'; c <='f'; c++)
-        putchar(c);
-    putchar('\n');
+	char c;
 
-    return (0);
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
 }
