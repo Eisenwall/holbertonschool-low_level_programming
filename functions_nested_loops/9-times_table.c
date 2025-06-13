880db1fd199ce6abaac8d443c182fee0e493c7aa
Eisenwall <Eisenwall@proton.me>
2025-06-13 14:04:02 +0000

    HTS

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,33 +5,33 @@
  */
 void times_table(void)
 {
-    int i, j, product;
+	int i, j, product;
 
-    for (i = 0; i <= 9; i++)  /* строки 0..9 */
-    {
-        for (j = 0; j <= 9; j++)  /* столбцы 0..9 */
-        {
-            product = i * j;
+	for (i = 0; i <= 9; i++) /* строки 0..9 */
+	{
+		for (j = 0; j <= 9; j++) /* столбцы 0..9 */
+		{
+			product = i * j;
 
-            if (j == 0)
-            {
-                _putchar(product + '0');
-            }
-            else if (product < 10)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-                _putchar(product + '0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar((product / 10) + '0');
-                _putchar((product % 10) + '0');
-            }
-        }
-        _putchar('\n');
-    }
+			if (j == 0)
+			{
+				_putchar(product + '0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
