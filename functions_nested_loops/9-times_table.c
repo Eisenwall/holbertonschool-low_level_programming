0bace474ebb6626eb54a6d208a270a495ee474ea
Eisenwall <Eisenwall@proton.me>
2025-06-13 14:02:48 +0000

    HTS

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,28 +7,27 @@ void times_table(void)
 {
     int i, j, product;
 
-    for (i = 0; i <= 9; i++)  // строки 0..9
+    for (i = 0; i <= 9; i++)  /* строки 0..9 */
     {
-        for (j = 0; j <= 9; j++)  // столбцы 0..9
+        for (j = 0; j <= 9; j++)  /* столбцы 0..9 */
         {
             product = i * j;
 
             if (j == 0)
             {
-                _putchar(product + '0');  // первый элемент без запятой и пробелов
+                _putchar(product + '0');
             }
             else if (product < 10)
             {
                 _putchar(',');
                 _putchar(' ');
                 _putchar(' ');
-                _putchar(product + '0');  // если результат однозначный — 2 пробела перед числом
+                _putchar(product + '0');
             }
             else
             {
                 _putchar(',');
                 _putchar(' ');
-                // если результат двузначный, печатаем две цифры
                 _putchar((product / 10) + '0');
                 _putchar((product % 10) + '0');
             }
