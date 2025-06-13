e8012677c48eb0bc6e70d65bf77fbbd351b98091
Eisenwall <Eisenwall@proton.me>
2025-06-13 12:35:10 +0000

    HTS

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,10 +9,13 @@
  */
 int main(void)
 {
-    char c;
-    for (c = '0'; c <= '9'; c++){
-        putchar(c);
-        putchar(', ');
+    int c;
+    for (c = 0; c <= 9; c++){
+        putchar('0' + c);
+        if (c != 9){
+            putchar(',');
+            putchar(' ');
+        }
     }
     putchar('\n');
     return (0);
