8fef3fb6d9b5e95bb5bdbb59c6d813201e05bf76
Eisenwall <Eisenwall@proton.me>
2025-06-13 12:06:35 +0000

    HTS

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * hello world
+ * 
+ * 
+ */
+int main(void)
+{
+    char c;
+    for (c = 'z'; c >= 'a'; c++)
+        putchar(c);
+    putchar('\n');
+
+    return (0);
+}
