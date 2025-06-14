45e81f519176034d55b2139a259e55245a46af52
Eisenwall <Eisenwall@proton.me>
2025-06-14 12:29:51 +0000

    HTS

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -6,55 +6,54 @@
  */
 void print_times_table(int n)
 {
-    int i, j, product;
+	int i, j, product;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+		return;
 
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            product = i * j;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			product = i * j;
 
-            if (j == 0)
-            {
-                /* print first number without leading spaces or comma */
-                if (product >= 10)
-                {
-                    _putchar('0' + product / 10);
-                    _putchar('0' + product % 10);
-                }
-                else
-                    _putchar('0' + product);
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
+			if (j == 0)
+			{
+				/* print first number without leading spaces or comma */
+				if (product >= 10)
+				{
+					_putchar('0' + product / 10);
+					_putchar('0' + product % 10);
+				}
+				else
+					_putchar('0' + product);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 
-                /* print spaces to align numbers to width 3 */
-                if (product < 10)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar('0' + product);
-                }
-                else if (product < 100)
-                {
-                    _putchar(' ');
-                    _putchar('0' + product / 10);
-                    _putchar('0' + product % 10);
-                }
-                else /* product >= 100 */
-                {
-                    _putchar('0' + product / 100);
-                    _putchar('0' + (product / 10) % 10);
-                    _putchar('0' + product % 10);
-                }
-            }
-        }
-        _putchar('\n');
-    }
+				/* print spaces to align numbers to width 3 */
+				if (product < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + product);
+				}
+				else if (product < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + product / 10);
+					_putchar('0' + product % 10);
+				}
+				else /* product >= 100 */
+				{
+					_putchar('0' + product / 100);
+					_putchar('0' + (product / 10) % 10);
+					_putchar('0' + product % 10);
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }
-
