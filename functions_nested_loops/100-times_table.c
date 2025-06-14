8e54b20d3b1be3996a69cbe2219ca2ca4ebfeafe
Eisenwall <Eisenwall@proton.me>
2025-06-14 12:30:51 +0000

    HTS

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,12 +1,57 @@
 #include "main.h"
 
 /**
+ * print_number - prints a number with formatting for times table
+ * @num: number to print
+ * @is_first: 1 if first number in the row, 0 otherwise
+ */
+void print_number(int num, int is_first)
+{
+	if (is_first)
+	{
+		if (num >= 10)
+		{
+			_putchar('0' + num / 10);
+			_putchar('0' + num % 10);
+		}
+		else
+		{
+			_putchar('0' + num);
+		}
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+
+		if (num < 10)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + num);
+		}
+		else if (num < 100)
+		{
+			_putchar(' ');
+			_putchar('0' + num / 10);
+			_putchar('0' + num % 10);
+		}
+		else
+		{
+			_putchar('0' + num / 100);
+			_putchar('0' + (num / 10) % 10);
+			_putchar('0' + num % 10);
+		}
+	}
+}
+
+/**
  * print_times_table - prints the n times table starting with 0
- * @n: the size of the times table (0 to 15)
+ * @n: size of the times table (0 to 15)
  */
 void print_times_table(int n)
 {
-	int i, j, product;
+	int i, j;
 
 	if (n < 0 || n > 15)
 		return;
@@ -15,44 +60,7 @@ void print_times_table(int n)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			product = i * j;
-
-			if (j == 0)
-			{
-				/* print first number without leading spaces or comma */
-				if (product >= 10)
-				{
-					_putchar('0' + product / 10);
-					_putchar('0' + product % 10);
-				}
-				else
-					_putchar('0' + product);
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				/* print spaces to align numbers to width 3 */
-				if (product < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + product);
-				}
-				else if (product < 100)
-				{
-					_putchar(' ');
-					_putchar('0' + product / 10);
-					_putchar('0' + product % 10);
-				}
-				else /* product >= 100 */
-				{
-					_putchar('0' + product / 100);
-					_putchar('0' + (product / 10) % 10);
-					_putchar('0' + product % 10);
-				}
-			}
+			print_number(i * j, j == 0);
 		}
 		_putchar('\n');
 	}
