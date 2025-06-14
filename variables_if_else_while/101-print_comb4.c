fab82ace7c53f74543ac9c8ed63346bf86ab10b8
Eisenwall <Eisenwall@proton.me>
2025-06-14 11:10:55 +0000

    HTS

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -23,7 +23,7 @@ int main(void)
                 if (!(a == 7 && b == 8 && c == 9))
                 {
                     putchar(',');
-                    puthcar(' ');
+                    putchar(' ');
                 }
             }
         }
