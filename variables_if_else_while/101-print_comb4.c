c526f6850706cf410055905b3f666a7590dedf7e
Eisenwall <Eisenwall@proton.me>
2025-06-14 11:09:18 +0000

    HTS

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+    int a, b, c;
+
+    for (a = 0; a<= 7; a++)
+    {
+        for (b = a + 1; b <= 8; b++)
+        {
+            for (c = b + 1; c <= 9; c++)
+            {
+                putchar(a + 48);
+                putchar(b + 48);
+                putchar(c + 48);
+                if (!(a == 7 && b == 8 && c == 9))
+                {
+                    putchar(',');
+                    puthcar(' ');
+                }
+            }
+        }
+    }
+    putchar('\n');
+    return (0);
+}
