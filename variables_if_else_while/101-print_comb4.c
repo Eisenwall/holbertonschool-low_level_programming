4b17b2745e229542d7184ec52d3a977a4d92287a
Eisenwall <Eisenwall@proton.me>
2025-06-14 11:12:37 +0000

    HTS

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 
 /**
- * main - prints the lowercase alphabet in reverse
+ * main - prints all possible different combinations of three digits
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int a, b, c;
+	int a, b, c;
 
-    for (a = 0; a<= 7; a++)
-    {
-        for (b = a + 1; b <= 8; b++)
-        {
-            for (c = b + 1; c <= 9; c++)
-            {
-                putchar(a + 48);
-                putchar(b + 48);
-                putchar(c + 48);
-                if (!(a == 7 && b == 8 && c == 9))
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+	for (a = 0; a <= 7; a++)
+	{
+		for (b = a + 1; b <= 8; b++)
+		{
+			for (c = b + 1; c <= 9; c++)
+			{
+				putchar(a + 48);
+				putchar(b + 48);
+				putchar(c + 48);
+				if (!(a == 7 && b == 8 && c == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
