486d4c151a5d2a5359e55e1ea7076374589368a4
Eisenwall <Eisenwall@proton.me>
2025-06-14 12:38:21 +0000

    HTS

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - выводит сумму чётных чисел Фибоначчи, не превышающих 4 000 000
+ *
+ * Return: всегда 0
+ */
+int main(void)
+{
+	unsigned long fib1 = 1, fib2 = 2, next;
+	unsigned long sum_even = 2; /* учитываем 2, так как оно чётное */
+	
+	while (1)
+	{
+		next = fib1 + fib2;
+		if (next > 4000000)
+			break;
+		if (next % 2 == 0)
+			sum_even += next;
+		fib1 = fib2;
+		fib2 = next;
+	}
+	
+	printf("%lu\n", sum_even);
+
+	return (0);
+}
