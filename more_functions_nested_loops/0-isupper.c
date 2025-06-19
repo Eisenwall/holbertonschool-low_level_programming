8dce43e6b37da5bee059ca5cb9a462e3097be78b
Eisenwall <Eisenwall@proton.me>
2025-06-19 11:37:40 +0000

    FYA

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 
-
-int main()
+/**
+ * _isupper - Checks for uppercase character
+ * @c: The character to check (as int)
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
 {
-    int c;
     if (c >= 'A' && c <= 'Z')
-        return(1);
+        return (1);
     else
-        return(0);
+        return (0);
 }
