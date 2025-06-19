27466e7433c30b195981475dfd65d7de149ec126
Eisenwall <Eisenwall@proton.me>
2025-06-19 12:42:01 +0000

    FYA

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -7,11 +7,12 @@
  */
 void print_most_numbers(void)
 {
-    int i;
-    for (i = 0; i <= 9; i++)
-    {
-        if (i != 2 && i != 4)
-            _putchar(i + '0');  /* Первый вызов _putchar - вывод цифр */
-    }
-    _putchar('\n');           /* Второй вызов _putchar - вывод новой строки */
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
 }
