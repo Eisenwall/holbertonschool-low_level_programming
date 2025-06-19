6979b1734aaa9c2d87c404d6f803910e00c3c40e
Eisenwall <Eisenwall@proton.me>
2025-06-19 12:06:15 +0000

    FYA

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of multiplication
+ */
 int mul(int a, int b)
 {
 	return (a * b);
