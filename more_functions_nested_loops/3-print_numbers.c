05432b94ab3f43a8c4e59c5edabf25ea3c826c5d
Eisenwall <Eisenwall@proton.me>
2025-06-19 12:13:28 +0000

    FYA

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+    char i;
+    for (i = '0'; i <= 9; i++)
+        putchar(i);
+    putchar('\n');
+}
