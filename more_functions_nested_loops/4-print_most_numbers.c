a2de226a95b65cb093d73b7e93e25a22f6ee08ca
Eisenwall <Eisenwall@proton.me>
2025-06-19 12:26:27 +0000

    FYA

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+    char i;
+    for (i = '0'; i <= '9'; i++)
+        if (i != '2' && i != '4')
+            _putchar(i);
+    _putchar('\n');
+}
