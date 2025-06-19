a321830b8a2b32b03a0bbfc74093d0b9b1d6f045
Eisenwall <Eisenwall@proton.me>
2025-06-19 12:55:23 +0000

    FYA

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+    int i, j;
+    char tens, ones;
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j <= 14; i++)
+        {
+            tens = (j / 10) + '0';
+            ones = (j % 10) + '0';
+            if (j >= 10)
+                _putchar(tens);
+            puthcar(ones);
+        }
+        _putchar('\n');
+    }
+}
