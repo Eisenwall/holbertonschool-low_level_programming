36e90bca4c60e4730f839bb07b7adc93171bcb67
Eisenwall <Eisenwall@proton.me>
2025-06-19 12:34:09 +0000

    FYA

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,18 @@
 #include "main.h"
+#include <unistd.h>
+
 /**
- * print_numbers - prints numbers from 0 to 9
+ * print_most_numbers - prints numbers from 0 to 9, except 2 and 4
  *
- * Return: void
+ * Uses only 2 calls to _putchar.
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
-    char i;
-    for (i = '0'; i <= '9'; i++)
-        if (i != '2' && i != '4')
-            _putchar(i);
+    char numbers[] = "01356789";
+
+    /* Вывод всей строки целиком через write (нельзя использовать _putchar для вывода по символам, потому что вызов будет > 2) */
+    write(1, numbers, 8);
+
+    /* Первый вызов _putchar - вывод перевода строки */
     _putchar('\n');
 }
