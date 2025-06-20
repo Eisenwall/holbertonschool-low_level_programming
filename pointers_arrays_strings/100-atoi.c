674c4e793a02feaab1fe6aad1d7c953820a94ce6
Eisenwall <Eisenwall@proton.me>
2025-06-20 12:03:36 +0000

    FYA

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,44 +1,35 @@
-#include <limits.h>
-
 /**
  * _atoi - converts a string to an integer
  * @s: input string
  *
- * Return: integer represented by the string or 0 if none found
+ * Return: the integer value represented by the string, or 0 if none found
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, num_started = 0;
+	int i = 0, sign = 1, sign_count = 0, num_started = 0;
 	unsigned int result = 0;
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == '-' && !num_started)
-			sign = -sign;
-		else if (s[i] == '+' && !num_started)
-			;
+		if ((s[i] == '-' || s[i] == '+') && !num_started)
+		{
+			if (s[i] == '-')
+				sign_count++;
+		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			num_started = 1;
-			int digit = s[i] - '0';
-
-			/* Проверка переполнения */
-			if (sign == 1)
-			{
-				if (result > (INT_MAX - digit) / 10)
-					return (INT_MAX);
-			}
-			else
-			{
-				if (result > ((unsigned int)(-(INT_MIN + digit))) / 10)
-					return (INT_MIN);
-			}
-
-			result = result * 10 + digit;
+			result = result * 10 + (s[i] - '0');
 		}
 		else if (num_started)
+		{
 			break;
+		}
 		i++;
 	}
+
+	if (sign_count % 2 != 0)
+		sign = -1;
+
 	return (sign * (int)result);
 }
