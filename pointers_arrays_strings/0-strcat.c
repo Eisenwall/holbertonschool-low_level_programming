d4259853a9d91e3fa4ab343a0f2c6df3e45cb8df
Eisenwall <Eisenwall@proton.me>
2025-06-20 12:56:01 +0000

    FYA

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,22 @@
 char *_strcat(char *dest, char *src)
 {
-    int i = 0, j = 0;
+	int i = 0, j = 0;
 
-    /* Найти конец строки dest */
-    while (dest[i] != '\0')
-        i++;
+	/* Найти конец строки dest */
+	while (dest[i] != '\0')
+		i++;
 
-    /* Копировать src в конец dest */
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
+	/* Копировать src в конец dest */
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-    /* Завершающий нулевой символ */
-    dest[i] = '\0';
+	/* Завершающий нулевой символ */
+	dest[i] = '\0';
 
-    return dest;
+	return (dest);
 }
+
