46d0b3c32da46f31242977d9e2cbb509e307b071
Eisenwall <Eisenwall@proton.me>
2025-06-20 12:56:43 +0000

    FYA

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,23 @@
+/**
+ * _strcat - concatenates two strings
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string to append
+ *
+ * Description: This function appends the string pointed to by src to the
+ * end of the string pointed to by dest. The terminating null byte of dest
+ * is overwritten and a new terminating null byte is added.
+ *
+ * Return: pointer to the resulting string dest
+ */
 char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
 
-	/* Найти конец строки dest */
+	/* Find end of dest string */
 	while (dest[i] != '\0')
 		i++;
 
-	/* Копировать src в конец dest */
+	/* Append src to dest */
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
@@ -14,9 +25,8 @@ char *_strcat(char *dest, char *src)
 		j++;
 	}
 
-	/* Завершающий нулевой символ */
+	/* Null-terminate the resulting string */
 	dest[i] = '\0';
 
 	return (dest);
 }
-
