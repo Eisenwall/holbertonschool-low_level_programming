deb7750fb17d0f9143d1cf76f72eca8d18bcd1ef
Eisenwall <Eisenwall@proton.me>
2025-06-20 11:29:21 +0000

    FYA

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,9 +6,10 @@
  */
 void puts_half(char *str)
 {
-	int l = 0;
+	int l;
 	int s;
 
+	l = 0;
 	while (str[l] != '\0')
 		l++;
 
@@ -23,29 +24,4 @@ void puts_half(char *str)
 		s++;
 	}
 	_putchar('\n');
-#include "main.h"
-
-/**
- *  * puts_half - prints the second half of a string
- *   * @str: input string
- *    */
-void puts_half(char *str)
-{
-	int l = 0;
-	int s;
-
-	while (str[l] != '\0')
-		l++;
-
-	if (l % 2 == 0)
-		s = l / 2;
-	else
-		s = (l + 1) / 2;
-
-	while (str[s] != '\0')
-	{
-			_putchar(str[s]);
-			s++;
-	}
-	_putchar('\n');
-}}
+}
