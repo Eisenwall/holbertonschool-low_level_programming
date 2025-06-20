5a6a47be62a0da8433e65430d6927d36c4a64120
Eisenwall <Eisenwall@proton.me>
2025-06-20 11:51:04 +0000

    FYA

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,3 @@
-#include "main.h"
-#include <stdio.h>
 /**
  * _strcpy - copies the string pointed to by src including the null byte
  * @dest: pointer to the destination buffer
@@ -11,13 +9,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
 
-    dest[i] = '\0';
-
-    return (dest);
+	return (dest);
 }
