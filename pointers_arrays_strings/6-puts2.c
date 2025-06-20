dbc5c3bd8f03645fc4ff77cbe7a532d27e122867
Eisenwall <Eisenwall@proton.me>
2025-06-20 11:04:36 +0000

    FYA

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, starting with first char
+ * puts2 - prints every other character of a string, starting with the first
  * @str: input string
  */
 void puts2(char *str)
@@ -10,8 +10,9 @@ void puts2(char *str)
 
     while (str[i] != '\0')
     {
-        _putchar(str[i]);
-        i += 2;
+        if (i % 2 == 0)
+            _putchar(str[i]);
+        i++;
     }
     _putchar('\n');
 }
