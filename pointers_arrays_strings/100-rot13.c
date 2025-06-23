1b695707f1282ec9e2d7646fa139b0b20f97b901
Eisenwall <Eisenwall@proton.me>
2025-06-23 13:21:34 +0000

    FYA

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -8,21 +8,21 @@
  */
 char *rot13(char *s)
 {
-    char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    char rot[]   = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-    int i, j;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; j < 52; j++) /* всего 52 буквы: 26 верхних + 26 нижних */
-        {
-            if (s[i] == alpha[j]) /* <-- Только один if */
-            {
-                s[i] = rot[j];
-                break;
-            }
-        }
-    }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = rot[j];
+				break;
+			}
+		}
+	}
 
-    return (s);
+	return (s);
 }
