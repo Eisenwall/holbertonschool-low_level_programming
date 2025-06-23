7f5c7b170ce9903c32157f61cba97f684d6c4686
Eisenwall <Eisenwall@proton.me>
2025-06-23 14:50:12 +0000

    FYA

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -17,6 +17,7 @@ char *cap_string(char *);
 void print_number(int n);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 
 
