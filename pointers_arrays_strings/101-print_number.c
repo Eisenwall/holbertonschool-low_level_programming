0e528d9bf7d9e33d1cf5fbad0bf127cef0e38a37
Eisenwall <Eisenwall@proton.me>
2025-06-23 13:23:01 +0000

    FYA

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer using only _putchar
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+	int pow10 = 1;
+	int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+
+	while ((num / pow10) >= 10)
+		pow10 *= 10;
+
+	while (pow10 > 0)
+	{
+		_putchar('0' + (num / pow10));
+		num %= pow10;
+		pow10 /= 10;
+	}
+}
