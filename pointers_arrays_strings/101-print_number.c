9b0df95420d7a0495d075c637eb753549a141fce
Eisenwall <Eisenwall@proton.me>
2025-06-23 13:31:17 +0000

    FYA

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,34 +1,30 @@
 #include "main.h"
 
-/**
- * print_number - prints an integer using _putchar
- * @n: number to print
- */
 void print_number(int n)
 {
-	int power = 1;
-	int temp = n;
+    int power = 1;
+    int temp = n;
 
-	if (n < 0)
-	{
-		_putchar('-');
+    if (n < 0)
+    {
+        _putchar('-');
+        if (n == -2147483648)
+        {
+            _putchar('2');
+            n = 147483648;
+        }
+        else
+        {
+            n = -n;
+        }
+    }
 
-		/* Обход переполнения при n == INT_MIN */
-		if (n == -2147483648)
-		{
-			_putchar('2');
-			n = 147483648;
-		}
-		else
-			n = -n;
-	}
+    while ((temp /= 10))
+        power *= 10;
 
-	while ((temp /= 10))
-		power *= 10;
-
-	while (power > 0)
-	{
-		_putchar((n / power) % 10 + '0');
-		power /= 10;
-	}
+    while (power > 0)
+    {
+        _putchar((n / power) % 10 + '0');
+        power /= 10;
+    }
 }
