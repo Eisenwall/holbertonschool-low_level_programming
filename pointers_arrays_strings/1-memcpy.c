87a2e1483fd1d58108e95b42ac6ed6fc2ea90dfd
Eisenwall <Eisenwall@proton.me>
2025-06-23 14:28:44 +0000

    FYA

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,3 +1,14 @@
+/**
+ * _memcpy - copies memory area
+ * @dest: pointer to the destination memory area
+ * @src: pointer to the source memory area
+ * @n: number of bytes to copy
+ *
+ * Description: This function copies n bytes from memory area src to
+ * memory area dest. It returns a pointer to dest.
+ *
+ * Return: pointer to dest
+ */
 char	*_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
