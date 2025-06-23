752cbd53ac5a32a51196ff56d8161da45bccdd61
Eisenwall <Eisenwall@proton.me>
2025-06-23 15:34:02 +0000

    FYA

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,14 +10,14 @@
  */
 void print_diagsums(int *a, int size)
 {
-    int i;
-    int sum_main = 0, sum_secondary = 0;
+	int i;
+	int sum_main = 0, sum_secondary = 0;
 
-    for (i = 0; i < size; i++)
-    {
-        sum_main += *(a + i * size + i);
-        sum_secondary += *(a + i * size + (size - 1 - i));
-    }
+	for (i = 0; i < size; i++)
+	{
+		sum_main += *(a + i * size + i);
+		sum_secondary += *(a + i * size + (size - 1 - i));
+	}
 
-    printf("%d, %d\n", sum_main, sum_secondary);
+	printf("%d, %d\n", sum_main, sum_secondary);
 }
