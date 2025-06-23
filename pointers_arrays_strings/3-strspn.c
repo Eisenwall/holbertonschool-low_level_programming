2c5b37551f8455eb057719e92238ed152f24e174
Eisenwall <Eisenwall@proton.me>
2025-06-23 15:07:07 +0000

    FYA

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,24 +1,35 @@
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to be scanned
+ * @accept: string containing the characters to match
+ *
+ * Description: This function returns the number of bytes in the
+ * initial segment of s which consist only of bytes from accept.
+ *
+ * Return: the number of bytes in the initial segment of s
+ *         consisting only of bytes from accept
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i = 0, j;
-    int found;
+	unsigned int i = 0, j;
+	int found;
 
-    while (s[i] != '\0')
-    {
-        found = 0;
-        j = 0;
-        while (accept[j] != '\0')
-        {
-            if (s[i] == accept[j])
-            {
-                found = 1;
-                break;
-            }
-            j++;
-        }
-        if (found == 0)
-            break;
-        i++;
-    }
-    return (i);
+	while (s[i] != '\0')
+	{
+		found = 0;
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				break;
+			}
+			j++;
+		}
+		if (found == 0)
+			break;
+		i++;
+	}
+	return (i);
 }
