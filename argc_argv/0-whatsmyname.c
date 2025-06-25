9fae27868ffd4703a8cd5da2a21a59bcf75231d5
Eisenwall <Eisenwall@proton.me>
2025-06-25 12:44:30 +0000

    commmit

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Prints the name of the program (with path)
  * @argc: argument count (unused)
@@ -9,6 +10,8 @@
 int main(int argc, char *argv[])
 {
     (void)argc;
+
     printf("%s\n", argv[0]);
+
     return (0);
 }
