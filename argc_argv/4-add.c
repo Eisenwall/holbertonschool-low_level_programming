1a5e0ade3bbeca46f2d6ac4d5731e1746ef72945
Eisenwall <Eisenwall@proton.me>
2025-06-25 13:28:21 +0000

    commmit

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h> /* необходимо подключить для isdigit */
+#include <ctype.h> /* нужно для isdigit */
 
 /**
  * main - adds positive numbers passed as arguments
@@ -11,26 +11,26 @@
  */
 int main(int argc, char *argv[])
 {
-    int sum = 0, i, j;
+	int sum = 0, i, j;
 
-    if (argc == 1)
-    {
-        printf("0\n");
-        return (0);
-    }
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j]; j++)
-        {
-            if (!isdigit(argv[i][j]))
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
-        sum += atoi(argv[i]);
-    }
-    printf("%d\n", sum);
-    return (0);
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
