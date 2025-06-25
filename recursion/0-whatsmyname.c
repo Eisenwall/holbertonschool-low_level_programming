59db9af80b09257433da37d3e9ad61a2ca6096b3
Eisenwall <Eisenwall@proton.me>
2025-06-25 12:31:43 +0000

    FuUA

---
+++ recursion/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * wildcmp - сравнивает строки, где s2 может содержать '*'
+ * @s1: первая строка
+ * @s2: вторая строка, может содержать '*'
+ * Return: 1 если строки совпадают, иначе 0
+ */
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    printf("%s\n", argv[0]);
+    return (0);
+}
