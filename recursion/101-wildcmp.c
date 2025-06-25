17c7458b9b44a2765b94de14fb50f40ee5cd18aa
Eisenwall <Eisenwall@proton.me>
2025-06-25 11:26:06 +0000

    FuUA

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings, where * can replace any string
+ * wildcmp - compares two strings allowing * as wildcard
  * @s1: first string
- * @s2: second string (can contain *)
- *
- * Return: 1 if the strings can be considered identical, 0 otherwise
+ * @s2: second string (can include *)
+ * Return: 1 if identical, 0 if not
  */
 int wildcmp(char *s1, char *s2)
 {
@@ -14,26 +13,18 @@ int wildcmp(char *s1, char *s2)
 
 	if (*s2 == '*')
 	{
-		/* skip all consecutive '*' */
-		while (*s2 == '*')
+		// Пропускаем все подряд идущие * (например, "**" -> "*")
+		while (*(s2 + 1) == '*')
 			s2++;
 
-		if (*s2 == '\0') /* "*" at end can match anything */
-			return (1);
-
-		/* try to match * with current s1 char or skip one s1 char */
-		while (*s1 != '\0')
-		{
-			if (wildcmp(s1, s2))
-				return (1);
-			s1++;
-		}
-
-		return (0); /* * couldn't match the rest */
+		// '*' может соответствовать пустой строке или одному символу из s1
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 	}
 
+	// Если символы равны, продолжаем сравнивать дальше
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 
+	// Если символы не равны и нет *, возвращаем 0
 	return (0);
 }
