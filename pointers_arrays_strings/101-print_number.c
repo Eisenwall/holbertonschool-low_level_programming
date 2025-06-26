b9041c413343e6520439cb1ca56673ae4ad729af
Eisenwall <Eisenwall@proton.me>
2025-06-26 09:05:59 +0000

    FYA

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -7,23 +7,23 @@
  */
 void print_number(int n)
 {
-    if (n == INT_MIN)  /* Special case for INT_MIN */
-    {
-        _putchar('-');
-        _putchar('2');
-        n = 147483648;  /* Convert to positive number to print */
-    }
+	if (n == INT_MIN)  /* Special case for INT_MIN */
+	{
+		_putchar('-');
+		_putchar('2');
+		n = 147483648;  /* Convert to positive number to print */
+	}
 
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;  /* Make the number positive for further processing */
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;  /* Make the number positive for further processing */
+	}
 
-    if (n / 10 != 0)
-    {
-        print_number(n / 10);  /* Recursively call with the quotient */
-    }
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);  /* Recursively call with the quotient */
+	}
 
-    _putchar((n % 10) + '0');  /* Print the last digit by getting remainder */
+	_putchar((n % 10) + '0');  /* Print the last digit by getting remainder */
 }
