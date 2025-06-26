25f0ab09a5e154726181143ecb10df8d7120d571
Eisenwall <Eisenwall@proton.me>
2025-06-26 08:49:03 +0000

    commmit

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -12,27 +12,22 @@ int main(int argc, char *argv[])
     int cents, coins = 0;
     int denominations[] = {25, 10, 5, 2, 1};
     int i;
-
     if (argc != 2)
     {
         printf("Error\n");
         return (1);
     }
-
     cents = atoi(argv[1]);
-
     if (cents < 0)
     {
         printf("0\n");
         return (0);
     }
-
     for (i = 0; i < 5; i++)
     {
         coins += cents / denominations[i];
         cents %= denominations[i];
     }
-
     printf("%d\n", coins);
     return (0);
 }
