a560529367d59934cafcb61f1ad35167475fa25c
Eisenwall <Eisenwall@proton.me>
2025-07-01 13:04:43 +0000

    FuUA

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,44 +1,37 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings into a newly allocated space
- * @s1: first string (can be NULL)
- * @s2: second string (can be NULL)
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: width of the grid (number of columns)
+ * @height: height of the grid (number of rows)
  *
- * Return: pointer to newly allocated concatenated string,
- *         or NULL if allocation fails
+ * Return: pointer to the 2D array, or NULL on failure or if width/height <= 0
  */
-char *str_concat(char *s1, char *s2)
+int **alloc_grid(int width, int height)
 {
-	char *concat;
-	unsigned int i, j, len1, len2;
+	int **grid;
+	int i, j;
 
-	/* Если NULL, считаем пустой строкой */
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	/* Вычисляем длины */
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-		;
-
-	/* Выделяем память под новую строку + '\0' */
-	concat = malloc((len1 + len2 + 1) * sizeof(char));
-	if (concat == NULL)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	/* Копируем s1 */
-	for (i = 0; i < len1; i++)
-		concat[i] = s1[i];
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+		return (NULL);
 
-	/* Копируем s2 */
-	for (j = 0; j < len2; j++)
-		concat[i + j] = s2[j];
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
 
-	concat[i + j] = '\0';
-
-	return (concat);
+	return (grid);
 }
