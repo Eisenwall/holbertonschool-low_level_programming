33ed7a81ca9c0ef2b773aee2cd3c52c4de0eb698
Eisenwall <Eisenwall@proton.me>
2025-07-01 13:20:15 +0000

    FuUA

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-
 /**
  * count_words - counts the number of words in a string
  * @str: input string
@@ -9,7 +8,6 @@
 int count_words(char *str)
 {
 	int i = 0, count = 0;
-
 	while (str[i] != '\0')
 	{
 		while (str[i] == ' ' && str[i] != '\0')
@@ -23,7 +21,6 @@ int count_words(char *str)
 	}
 	return (count);
 }
-
 /**
  * extract_word - allocates memory and copies a word 
  * from str[start] to str[end - 1]
@@ -37,17 +34,13 @@ char *extract_word(char *str, int start, int end)
 {
 	int i;
 	char *word = malloc((end - start + 1) * sizeof(char));
-
 	if (word == NULL)
 		return (NULL);
-
 	for (i = 0; start < end; start++, i++)
 		word[i] = str[start];
-
 	word[i] = '\0';
 	return (word);
 }
-
 /**
  * strtow - splits a string into words
  * @str: the string to split
@@ -58,18 +51,14 @@ char **strtow(char *str)
 {
 	char **words;
 	int i = 0, j = 0, start, end, wc;
-
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
-
 	wc = count_words(str);
 	if (wc == 0)
 		return (NULL);
-
 	words = malloc((wc + 1) * sizeof(char *));
 	if (words == NULL)
 		return (NULL);
-
 	while (str[i] != '\0' && j < wc)
 	{
 		while (str[i] == ' ')
@@ -78,7 +67,6 @@ char **strtow(char *str)
 		while (str[i] != ' ' && str[i] != '\0')
 			i++;
 		end = i;
-
 		words[j] = extract_word(str, start, end);
 		if (words[j] == NULL)
 		{
