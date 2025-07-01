fba671762e929d533bc98e9e8bef3ddeb146e8df
Eisenwall <Eisenwall@proton.me>
2025-07-01 13:23:31 +0000

    FuUA

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+
 /**
  * count_words - counts the number of words in a string
  * @str: input string
@@ -8,10 +9,12 @@
 int count_words(char *str)
 {
 	int i = 0, count = 0;
+
 	while (str[i] != '\0')
 	{
 		while (str[i] == ' ' && str[i] != '\0')
 			i++;
+
 		if (str[i] != ' ' && str[i] != '\0')
 		{
 			count++;
@@ -19,31 +22,39 @@ int count_words(char *str)
 				i++;
 		}
 	}
+
 	return (count);
 }
+
 /**
- * extract_word - allocates memory and copies a word 
- * from str[start] to str[end - 1]
+ * extract_word - allocates memory and copies a word
+ *                from str[start] to str[end - 1]
  * @str: source string
  * @start: start index
  * @end: end index (exclusive)
  *
- * Return: pointer to the new word or NULL on failure
+ * Return: pointer to new word or NULL
  */
 char *extract_word(char *str, int start, int end)
 {
 	int i;
-	char *word = malloc((end - start + 1) * sizeof(char));
+	char *word;
+
+	word = malloc((end - start + 1) * sizeof(char));
 	if (word == NULL)
 		return (NULL);
+
 	for (i = 0; start < end; start++, i++)
 		word[i] = str[start];
+
 	word[i] = '\0';
+
 	return (word);
 }
+
 /**
  * strtow - splits a string into words
- * @str: the string to split
+ * @str: input string
  *
  * Return: pointer to array of words, or NULL
  */
@@ -51,14 +62,18 @@ char **strtow(char *str)
 {
 	char **words;
 	int i = 0, j = 0, start, end, wc;
+
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
+
 	wc = count_words(str);
 	if (wc == 0)
 		return (NULL);
+
 	words = malloc((wc + 1) * sizeof(char *));
 	if (words == NULL)
 		return (NULL);
+
 	while (str[i] != '\0' && j < wc)
 	{
 		while (str[i] == ' ')
@@ -67,16 +82,21 @@ char **strtow(char *str)
 		while (str[i] != ' ' && str[i] != '\0')
 			i++;
 		end = i;
+
 		words[j] = extract_word(str, start, end);
 		if (words[j] == NULL)
 		{
 			while (j > 0)
-				free(words[--j]);
+			{
+				j--;
+				free(words[j]);
+			}
 			free(words);
 			return (NULL);
 		}
 		j++;
 	}
 	words[j] = NULL;
+
 	return (words);
 }
