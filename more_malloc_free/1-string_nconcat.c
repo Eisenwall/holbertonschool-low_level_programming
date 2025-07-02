627aed36e0b0dd89ffe538e04c42b3bc545c3097
Eisenwall <Eisenwall@proton.me>
2025-07-02 12:13:04 +0000

    FuUA

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * string_nconcat - concatenates s1 with first n bytes of s2
@@ -8,34 +7,34 @@
  * @s2: second string (can be NULL)
  * @n: max number of bytes to take from s2
  *
- * Return: pointer to newly allocated string, or NULL on failure
+ * Return: pointer to new string, or NULL on failure
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int len1, len2, len_total;
+    unsigned int len1 = 0, len2 = 0, i;
     char *result;
 
     if (s1 == NULL)
         s1 = "";
     if (s2 == NULL)
         s2 = "";
-
-    len1 = strlen(s1);
-    len2 = strlen(s2);
+    while (s1[len1] != '\0')
+        len1++;
+    while (s2[len2] != '\0')
+        len2++;
 
     if (n >= len2)
         n = len2;
 
-    len_total = len1 + n;
-
-    result = malloc(len_total + 1);
+    result = malloc(len1 + n + 1);
     if (result == NULL)
         return (NULL);
+    for (i = 0; i < len1; i++)
+        result[i] = s1[i];
+    for (i = 0; i < n; i++)
+        result[len1 + i] = s2[i];
 
-    memcpy(result, s1, len1);
-    memcpy(result + len1, s2, n);
-
-    result[len_total] = '\0';
+    result[len1 + n] = '\0';
 
     return (result);
 }
