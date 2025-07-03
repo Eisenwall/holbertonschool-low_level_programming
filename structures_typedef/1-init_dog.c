2b1268bf77b07abead699b3d947c140176017346
Eisenwall <Eisenwall@proton.me>
2025-07-03 10:40:00 +0000

    FuUA

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
-#include "dog.h"
 #include <stddef.h>
+#include "dog.h"
 
 /**
  * init_dog - инициализирует структуру dog
@@ -10,10 +10,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d == NULL)
-        return;
+	if (d == NULL)
+		return;
 
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
