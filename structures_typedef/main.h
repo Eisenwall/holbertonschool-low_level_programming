cc1ecaa5f9b4dbf6285fc7fe8f748f6282204036
Eisenwall <Eisenwall@proton.me>
2025-07-03 10:36:51 +0000

    FuUA

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,19 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
-
-/**
- * struct dog - структура для хранения информации о собаке
- * @name: имя собаки
- * @age: возраст собаки
- * @owner: владелец собаки
- */
-struct dog
-{
-    char *name;
-    float age;
-    char *owner;
-};
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
-#endif /* DOG_H */
+
+
+
+#endif
