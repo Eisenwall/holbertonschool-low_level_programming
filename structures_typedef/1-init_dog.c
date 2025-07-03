078537001764536eb9e8e240849af800d3cd001b
Eisenwall <Eisenwall@proton.me>
2025-07-03 09:56:36 +0000

    FuUA

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,12 +1,6 @@
+#include <stddef.h>
 #include "dog.h"
 
-/**
- * init_dog - инициализирует переменную типа struct dog
- * @d: указатель на структуру dog
- * @name: имя собаки
- * @age: возраст собаки
- * @owner: владелец собаки
- */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
     if (d == NULL)
