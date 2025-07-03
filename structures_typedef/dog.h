166e91baa6efc095fa5e0547717edaa4e45b0279
Eisenwall <Eisenwall@proton.me>
2025-07-03 09:50:47 +0000

    FuUA

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog - structure for a dog
+ * @name: pointer to dog's name (string)
+ * @age: dog's age (float)
+ * @owner: pointer to dog's owner name (string)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
