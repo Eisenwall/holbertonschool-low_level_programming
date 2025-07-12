a6a93bb0cbfe60c0053c3cbcce80795ceee84681
Eisenwall <Eisenwall@proton.me>
2025-07-12 15:30:15 +0000

    FP

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,10 @@
-#include <stdio.h>
+#include "function_pointers.h"
 
+/**
+ * print_name - function to print a name
+ * *name the name to print
+ * *f function pointer
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
+void print_name(char *name, void (*f)(char *));
 
-void print_name(char* name, void (*f)(char *))
-{
-        f(name);
-}
+
+
+#endif
