512f59ff3c56c9cfac3922437f25c2575042475c
Eisenwall <Eisenwall@proton.me>
2025-07-12 17:03:38 +0000

    FP

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,23 +1,14 @@
-#include "function_pointers.h"
+#include <stdio.h>
 
-/**
- * int_index - searches for an integer in an array
- * @array: pointer to the array
- * @size: number of elements
- * @cmp: pointer to the function
- */
 int int_index(int *array, int size, int (*cmp)(int))
 {
     int i;
-
-    if (array == NULL || cmp == NULL || size <= 0)
-        return (-1);
-
+    if (size <= 0)
+        return(-1);
     for (i = 0; i < size; i++)
     {
         if (cmp(array[i]) != 0)
-            return (i);
+            return(0);
     }
-
-    return (-1);
+    return(-1);
 }
