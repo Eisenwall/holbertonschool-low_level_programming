875ee5c684468727127a6efd38d737d40274c790
Eisenwall <Eisenwall@proton.me>
2025-07-12 17:21:36 +0000

    FP

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -4,8 +4,7 @@
  * int_index - searches for an integer in an array using a comparison function
  * @array: pointer to the array of integers
  * @size: number of elements in the array
- * @cmp: pointer to the comparison function that returns 0 if no match, non-zero otherwise
- *
+ * @cmp: pointer to the comparison function that returns 0 if no match
  * Return: index of the first element for which cmp returns non-zero,
  *         or -1 if no such element is found or if size <= 0
  */
