c460521cb0dc10add6f3437f79f2863364bcf908
Eisenwall <Eisenwall@proton.me>
2025-07-12 16:57:25 +0000

    FP

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+    if (size <= 0)
+        return(-1);
+    for (int i = 0; i < size; i++)
+    {
+        if (cmp(array[i]) != 0)
+            return(0);
+    }
+    return(-1);
+}
