d6953e336c17c496ffe11b13c44a2d54e1b0cf8d
Eisenwall <Eisenwall@proton.me>
2025-07-12 15:33:52 +0000

    FP

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,8 +2,8 @@
 
 /**
  * print_name - function to print a name
- * *name the name to print
- * *f function pointer
+ * @name: the name to print
+ * @f: function pointer that takes an char * argument
  */
 void print_name(char *name, void (*f)(char *))
 {
