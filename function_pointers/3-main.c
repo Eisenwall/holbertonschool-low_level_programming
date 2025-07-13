15e03d7bdad9e0b339925385f917d8b36a85223f
Eisenwall <Eisenwall@proton.me>
2025-07-13 07:23:58 +0000

    FP

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,36 +2,42 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+/**
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*op_func)(int, int);
+	int a, b, result;
+	int (*op_func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-    op_func = get_op_func(argv[2]);
+	op_func = get_op_func(argv[2]);
 
-    if (op_func == NULL)
-    {
-        printf("Error\n");
-        return (99);
-    }
+	if (op_func == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        return (100);
-    }
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    result = op_func(num1, num2);
-    printf("%d\n", result);
+	result = op_func(a, b);
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
