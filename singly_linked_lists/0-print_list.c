8c9e9c9793986298092197f38f58e876af6d378c
Eisenwall <Eisenwall@proton.me>
2025-08-07 10:38:02 +0000

    HTS

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+i#include "lists.h"
 #include <stdio.h>
 
 /**
@@ -9,18 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
-        
-        count++;
-        h = h->next;
-    }
-    
-    return count;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		count++;
+		h = h->next;
+	}
+
+	return (count);
 }
