356d63d343ede1edbe308809ac72ec3aea748506
Eisenwall <Eisenwall@proton.me>
2025-08-14 08:05:05 +0000

    HTS

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
-/*
-*list_len - returns the number of elements in a linked list
-*@h: pointer to the head of the list
-*Return: number of elements
-*/
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes
+ */
 size_t list_len(const list_t *h)
 {
-    size_t c = 0;
+	size_t count = 0;
 
-    while (h != NULL)
-    {
-        h = h->next;
-        c++;
-    }
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
 
-    return (c);
+	return (count);
 }
