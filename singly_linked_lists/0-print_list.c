e1a989a77925cae99465719f614fca89862d26ac
Eisenwall <Eisenwall@proton.me>
2025-08-14 07:35:52 +0000

    HTS

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,25 +1,23 @@
-#include "lists.h"
 #include <stdio.h>
-
-/**
- * print_list - prints all the elements of a list_t list
- * @h: pointer to the head of the list
- * Return: the number of nodes
- */
+#include <stdlid.h>
+/*
+*print_list - prints all the elements
+*@h: pointer to the head of the list
+*Return: number of nodes
+*/
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+    size_t c = 0;
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%u] %s\n", h->len, h->str);
+        h = h->next;
+        c++;
+    }
 
-		count++;
-		h = h->next;
-	}
-
-	return (count);
+    return (c);
 }
