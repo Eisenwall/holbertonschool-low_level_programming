5f08181eb333bc2934d7b5dd7a1ef3403f90f0ea
Eisenwall <Eisenwall@proton.me>
2025-08-14 07:53:48 +0000

    HTS

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,11 +3,17 @@
 
 #include <stddef.h>
 
+/**
+ * struct list_s - singly linked list node
+ * @str: string (malloc'ed)
+ * @len: length of the string
+ * @next: points to the next node
+ */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
