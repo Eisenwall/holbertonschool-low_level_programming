d9cf3275e59ed8ddd7ee0e7e629761bee68f09bf
Eisenwall <Eisenwall@proton.me>
2025-08-14 07:52:34 +0000

    HTS

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,12 +1,14 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stddef.h>
-struct Node {
+
+typedef struct list_s
+{
     char *str;
     unsigned int len;
     struct list_s *next;
-};
+} list_t;
 
 size_t print_list(const list_t *h);
 
